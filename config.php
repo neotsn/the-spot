a1914ae307598d052c43ff35086dfd5f532f2a3b
@neotsn <neotsn@gmail.com>
2015-07-20 02:09:23 +0000

    adding missing files from previous commit

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+
+require_once(PATH_ROOT.'vendor/autoload.php');
+require_once(PATH_ROOT.'src/includes/defines.php');
---
+++ src/includes/defines.php
@@ -0,0 +1,8 @@
+<?php
+
+// Paths
+define('PATH_SRC', PATH_ROOT.'src/');
+define('PATH_INCLUDES', PATH_SRC.'includes/');
+define('PATH_TEMPLATES', PATH_ROOT . 'templates/');
+define('PATH_CSS','/resources/Motif/build/css/');
+define('PATH_JS', '/resources/Motif/build/js/');
